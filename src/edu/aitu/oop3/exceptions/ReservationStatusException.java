387da7e06240eb3c152512a6c2b290185f606c40
sketto <ashenov2022@gmail.com>
2026-01-25 19:45:45 +0500

    Add ReservationStatusException class

--- src/edu/aitu/oop3/exceptions/ReservationStatusException.java
+++ src/edu/aitu/oop3/exceptions/ReservationStatusException.java
@@ -1,1 +1,7 @@
+package edu.aitu.oop3.exceptions;
 
+public class ReservationStatusException extends RuntimeException {
+    public ReservationStatusException(String message) {
+        super(message);
+    }
+}
