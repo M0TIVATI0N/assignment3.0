04881d466c3c9725991953276003b445a584652e
sketto <ashenov2022@gmail.com>
2026-01-25 19:45:19 +0500

    Add NoFreeSpotsException class

--- src/edu/aitu/oop3/exceptions/NoFreeSpotsException.java
+++ src/edu/aitu/oop3/exceptions/NoFreeSpotsException.java
@@ -1,1 +1,7 @@
+package edu.aitu.oop3.exceptions;
 
+public class NoFreeSpotsException extends RuntimeException {
+    public NoFreeSpotsException(String message) {
+        super(message);
+    }
+}
